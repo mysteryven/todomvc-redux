5cc6dbdf19221b6a07142094f38213c94ea3dfe3
wenzhe <mysteryven@gmail.com>
2021-12-12 19:26:40 +0800

    重新打包

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,17 @@
 import Todos from './Todos/todos';
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux';
 import store from './store';
 
 
 function App() {
   return (
-    <Provider store={store} >
-    <div className="bg-white mx-auto mt-100 w-2/4">
-     <Todos />
-    </div>
+    <Provider store={store}>
+      <a target="_blank" className="text-lg pt-40 ml-8" href="https://github.com/mysteryven/todomvc-redux">点击查看源码</a>
+      <div className="bg-white mx-auto mt-100 w-2/4">
+        <Todos/>
+      </div>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
